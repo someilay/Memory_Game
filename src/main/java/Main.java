fdb0412b811ffaff012f5b2cd7f2d2bbcd9d0235
kolbasaegor <xxxstroganov@ya.ru>
2021-10-03 20:57:24 +0300

    Factory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
 package main.java;
 
+import main.java.gameObjects.*;
+
 public class Main {
     public static void main(String[] args) {
-        String a = "innopolis govno";
-        System.out.println("Roman M umerchik");
+        GameObjectCreator gameObjectCreator = new MirCreator();
+        GameObject gameObject = gameObjectCreator.create(0, 0);
+
+        System.out.println(gameObject.getClass());
     }
+
+
 }
