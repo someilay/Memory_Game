168e66dabe171afd0e274fdbe893903c10062012
makro <rscm544@gmail.com>
2021-10-03 20:38:32 +0300

    jhvjhv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         String a = "innopolis govno";
         System.out.println("Roman M umerchik");
+        int tt = 0;
+        System.out.println(tt);
     }
 }
