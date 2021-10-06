0d613a98999963693b4aeb5f271c70e4be2a2d0e
kolbasaegor <xxxstroganov@ya.ru>
2021-10-06 15:28:24 +0300

    BETA 1.2 (BUG FIXED )

--- src/main/java/engine/World.java
+++ src/main/java/engine/World.java
@@ -8,15 +8,16 @@ import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class World {
-    public static final int WORLD_SIZE_X = 8;
-    public static final int WORLD_SIZE_Y = 8;
+    public static final int WORLD_SIZE_X = 4;
+    public static final int WORLD_SIZE_Y = 4;
 
     private Tile lastOpenTile;
+    private Tile lastClickedTile;
     private int openedTiles;
     private Tile[][] board;
 
     private void initBoard(){
-        board = new Tile[WORLD_SIZE_X][WORLD_SIZE_X];
+        board = new Tile[WORLD_SIZE_Y][WORLD_SIZE_X];
         for (int i = 0; i < WORLD_SIZE_Y; i++) {
             board[i] = new Tile[WORLD_SIZE_X];
         }
@@ -24,27 +25,33 @@ public class World {
         TileCreator[] tileCreator = { new TileCreator1(), new TileCreator2(), new TileCreator3(), new TileCreator4() };
         ArrayList<Position> pos = new ArrayList<>(WORLD_SIZE_X * WORLD_SIZE_Y);
 
-        for (int i = 0; i < WORLD_SIZE_X; i++) {
-            for (int j = 0; j < WORLD_SIZE_Y; j++) {
-                pos.add(new Position(i, j));
+        for (int y = 0; y < WORLD_SIZE_Y; y++) {
+            for (int x = 0; x < WORLD_SIZE_X; x++) {
+                pos.add(new Position(x, y));
             }
         }
 
         for (TileCreator creator : tileCreator) {
-            for (int j = 0; j < WORLD_SIZE_X + WORLD_SIZE_Y; j++) {
+            for (int j = 0; j < WORLD_SIZE_X * WORLD_SIZE_Y / tileCreator.length; j++) {
                 Position position = pos.remove(randInt(0, pos.size() - 1));
-                board[position.x][position.y] = creator.create(position.x, position.y);
+                board[position.y][position.x] = creator.create(position.x, position.y);
             }
         }
     }
 
     public void init() {
         initBoard();
-        this.lastOpenTile = null;
+        lastOpenTile = null;
+        lastClickedTile = null;
+        openedTiles = 0;
     }
 
     public int getOpenedTiles() {
         return openedTiles;
+    }
+
+    public Tile getLastClickedTile() {
+        return lastClickedTile;
     }
 
     int randInt(int min, int max){
@@ -59,20 +66,21 @@ public class World {
         int x = position.x;
         int y = position.y;
 
+        lastClickedTile = board[y][x];
+
         if (lastOpenTile != null) {
-            if ( !lastOpenTile.getPosition().equals(position) ) {
-                if ( lastOpenTile.getType() == board[x][y].getType() && !board[x][y].isOpen() ) {
-                    board[x][y].open();
-                    openedTiles++;
+            if ( !board[y][x].isOpen()) {
+                if ( lastOpenTile.getType() == board[y][x].getType()) {
+                    board[y][x].open();
+                    openedTiles += 2;
                 } else {
-                    board[lastOpenTile.getPosition().x][lastOpenTile.getPosition().y].hide();
-                    openedTiles--;
+                    board[lastOpenTile.getPosition().y][lastOpenTile.getPosition().x].hide();
                 }
                 lastOpenTile = null;
             }
         } else {
-            board[x][y].open();
-            lastOpenTile = board[x][y];
+            board[y][x].open();
+            lastOpenTile = board[y][x];
         }
     }
 }
