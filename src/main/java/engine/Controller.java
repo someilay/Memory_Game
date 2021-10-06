8f77525d3885e634031012b5ef35a311720a0124
kolbasaegor <xxxstroganov@ya.ru>
2021-10-06 13:19:44 +0300

    World fix 5 & Response

--- src/main/java/engine/Controller.java
+++ src/main/java/engine/Controller.java
@@ -5,44 +5,24 @@ import main.java.ButtonNames;
 
 public class Controller {
     private World world;
-    private boolean isWin;
-    private boolean isExit;
 
     public Controller() {
-        setWin(false);
-        setExit(false);
         world = new World();
     }
 
-    public void setWin(boolean win) {
-        this.isWin = win;
-    }
-
-    public void setExit(boolean exit) {
-        this.isExit = exit;
-    }
-
-    public boolean getExit() {
-        return this.isExit;
-    }
-
-    public boolean getWin() {
-        return this.isWin;
-    }
-
-    private void winChek() {
-        this.isWin = (world.getOpenedTiles() == 64);
+    private boolean winChek() {
+        return (world.getOpenedTiles() == 64);
     }
 
     public Response request(Button button){
+        boolean isExit = false;
         if (button.getName() == ButtonNames.BEGIN)
             world.init();
         else if (button.getName() == ButtonNames.EXIT)
             isExit = true;
         else {
             world.update(button.getPosition());
-            winChek();
         }
-        return new Response(world, isWin, isExit);
+        return new Response(world, winChek(), isExit);
     }
 }
